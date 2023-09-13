e42d6fee54cb5c734d556114960d2553b34d5cea
a.savanovich <asavanster@gmail.com>
2023-09-13 23:48:06 +0300

    new "browser" test

---
+++ tests/html_game/game.test.js
@@ -0,0 +1,38 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import {JSDOM} from "jsdom";
+
+import gameFunction from "../../src/js/game.js";
+import settings from "../../src/js/settings.js";
+
+test("simple ai scenario", async () => {
+    const dom = await JSDOM.fromFile("src/index.html", {
+        url: "http://localhost/",
+    });
+    const doc = dom.window.document;
+    settings.externalId = "client1";
+    settings.cardsDeal = 1;
+    settings.seed = "h";
+    settings.maxScore = 3;
+    settings.clickAll = true;
+    const game = gameFunction(dom.window, doc, settings);
+
+    game.on("move", (move) => console.log(move));
+    game.on("draw", () => {});
+    game.on("shuffle", () => {});
+    game.on("discard", () => {});
+    game.on("changeCurrent", () => {});
+    game.on("clearPlayer", () => {});
+    game.join(0, "server", "server");
+    for (let i = 1; i < 4; ++i) {
+        const name = "client" + i;
+        game.join(i, name, name);
+    }
+    game.afterAllJoined();
+
+    game.on("gameover", () => {
+        const btnAdd = document.querySelector(".butInstall");
+        btnAdd.classList.remove("hidden2");
+    });
+    assert.ok(true, "Ended well");
+});
