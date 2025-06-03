e036e7e1826022514aa4eabfd396a605e0c211e0
a.savanovich <asavanster@gmail.com>
2025-06-03 19:04:34 +0200

    fix eslint

--- linters/eslint.config.js
+++ linters/eslint.config.js
@@ -1,12 +1,12 @@
 import js from "@eslint/js";
 import globals from "globals";
-import stylisticJs from '@stylistic/eslint-plugin-js';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
     {
         ...js.configs.recommended,
         files: ["src/**/*.js", "test/**/*.js"],
-        ignores: ["src/js/lib/*"],
+        ignores: ["src/js/lib/*", "tests/manual/supabase/docs/*"],
         languageOptions: {
             ecmaVersion: 2022,
             sourceType: "module",
@@ -20,10 +20,10 @@ export default [
     },
     {
         plugins: {
-            '@stylistic/js': stylisticJs
+            '@stylistic': stylistic
         },
         files: ["src/**/*.js", "test/**/*.js"],
-        ignores: ["src/js/lib/*"],
+        ignores: ["src/js/lib/*", "tests/manual/supabase/docs"],
         rules: {
             "prefer-const": ["error"],
             "require-await": ["error"],
@@ -34,36 +34,36 @@ export default [
             "brace-style": ["error", "1tbs"],
             "arrow-body-style": ["error"],
             "space-before-blocks": ["error", "always"],
-            "@stylistic/js/indent": [
+            "@stylistic/indent": [
                 "error",
-                4
+                4,
+                { "SwitchCase": 0 }
             ],
-            "@stylistic/js/linebreak-style": [
+            "@stylistic/linebreak-style": [
                 "error",
                 "windows"
             ],
-            "@stylistic/js/quotes": [
+            "@stylistic/quotes": [
                 "error",
                 "double"
             ],
-            "@stylistic/js/semi": [
+            "@stylistic/semi": [
                 "error",
                 "always"
             ],
-            "@stylistic/js/no-extra-semi": ["error"],
-            "@stylistic/js/keyword-spacing": ["error"],
-            "@stylistic/js/semi-spacing": ["error"],
-            "@stylistic/js/no-trailing-spaces": ["error"],
-            "@stylistic/js/no-tabs": ["error"],
-            "@stylistic/js/max-len": ["error", {"code": 120}],
-            "@stylistic/js/no-whitespace-before-property": ["error"],
-            "@stylistic/js/no-mixed-spaces-and-tabs": ["error"],
-            "@stylistic/js/no-multi-spaces": ["error"],
-            "@stylistic/js/comma-spacing": ["error"],
-            "@stylistic/js/no-multi-spaces": ["error"]
+            "@stylistic/no-extra-semi": "error",
+            "@stylistic/keyword-spacing": ["error"],
+            "@stylistic/semi-spacing": ["error"],
+            "@stylistic/no-trailing-spaces": ["error"],
+            "@stylistic/no-tabs": ["error"],
+            "@stylistic/max-len": ["error", {"code": 120}],
+            "@stylistic/no-whitespace-before-property": ["error"],
+            "@stylistic/no-mixed-spaces-and-tabs": ["error"],
+            "@stylistic/comma-spacing": ["error"],
+            "@stylistic/no-multi-spaces": ["error"]
         }
     },
     {
-        ignores: ["old/*", "android/*", "docs/*", "src/js/lib/*"]
+        ignores: ["old/*", "android/*", "docs/*", "src/js/lib/*", "tests/manual/supabase/docs/*"]
     }
 ];
