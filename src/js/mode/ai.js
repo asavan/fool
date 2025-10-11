f13b137145147aaa989d6dc36f21c85be4727a10
a.savanovich <asavanster@gmail.com>
2025-10-11 09:25:44 +0300

    replace promise with async

--- src/js/mode/ai.js
+++ src/js/mode/ai.js
@@ -1,26 +1,24 @@
 import PromiseQueue from "../utils/async-queue.js";
 import loggerFunc from "../views/logger.js";
 
-export default function ai(window, document, settings, gameFunction) {
-    return new Promise((resolve) => {
-        const playerName = "Player";
-        const myId = playerName;
-        const logger = loggerFunc(3, null, settings);
-        const queue = PromiseQueue(logger);
-        const game = gameFunction({window, document, settings, myId});
-        game.setQueue(queue);
+export default async function ai(window, document, settings, gameFunction) {
+    const playerName = "Player";
+    const myId = playerName;
+    const logger = loggerFunc(3, null, settings);
+    const queue = PromiseQueue(logger);
+    const game = gameFunction({window, document, settings, myId});
+    game.setQueue(queue);
 
-        game.join(playerName, playerName, settings.playerIsBot);
+    game.join(playerName, playerName, settings.playerIsBot);
 
-        for (let i = 0; i < settings.botCount; ++i) {
-            game.addBot();
-        }
+    for (let i = 0; i < settings.botCount; ++i) {
+        game.addBot();
+    }
 
-        game.on("gameover", () => {
-            const btnAdd = document.querySelector(".butInstall");
-            btnAdd.classList.remove("hidden2");
-        });
-        resolve(game);
-        game.afterAllJoined();
+    game.on("gameover", () => {
+        const btnAdd = document.querySelector(".butInstall");
+        btnAdd.classList.remove("hidden2");
     });
+    await game.afterAllJoined();
+    return game;
 }
--- src/js/mode/hotseat.js
+++ src/js/mode/hotseat.js
@@ -1,19 +1,17 @@
-export default function hotseat(window, document, settings, gameFunction) {
-    return new Promise((resolve) => {
-        const myId = "client1";
-        settings.clickAll = true;
-        const game = gameFunction({window, document, settings, myId});
-        game.join("server", "server");
-        for (let i = 1; i < settings.botCount + 1; ++i) {
-            const name = "client" + i;
-            game.join(name, name);
-        }
+export default async function hotseat(window, document, settings, gameFunction) {
+    const myId = "client1";
+    settings.clickAll = true;
+    const game = gameFunction({window, document, settings, myId});
+    game.join("server", "server");
+    for (let i = 1; i < settings.botCount + 1; ++i) {
+        const name = "client" + i;
+        game.join(name, name);
+    }
 
-        game.on("gameover", () => {
-            const btnAdd = document.querySelector(".butInstall");
-            btnAdd.classList.remove("hidden2");
-        });
-
-        game.afterAllJoined().then(() => resolve(game));
+    game.on("gameover", () => {
+        const btnAdd = document.querySelector(".butInstall");
+        btnAdd.classList.remove("hidden2");
     });
+    await game.afterAllJoined();
+    return game;
 }
